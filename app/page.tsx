e40f84489808561db2ee9ce55dc1d9d99345f68c
saketh <saketh.reddy@trainingmug.com>
2024-09-27 19:31:44 +0530

    basic layout of kaban board

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,6 @@ import "./globals.css";
 import { KanbanBoard } from "@/components/KabanBoard";
 import { Button } from "@/components/ui/button";
 
-const FooterLink = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Button
-      variant="link"
-      asChild
-      className="scroll-m-20 text-xl font-semibold tracking-tight"
-    >
-      {children}
-    </Button>
-  );
-};
 
 function App() {
   return (
